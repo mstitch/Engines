f435af003f9c79f368a17f0ee6d9bc49958400e4
mstitch <marcosvv11@gmail.com>
2020-06-03 01:33:14 -0300

    I.A Que atira e persegue

---
+++ Assets/script/Iaesqueleto.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Iaesqueleto : MonoBehaviour
+{
+   [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(_navMeshAgent == null)
+        {
+            Debug.LogError("DEU MERDA AQUI" + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+    private void SetDestination()
+    {
+        if(_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
