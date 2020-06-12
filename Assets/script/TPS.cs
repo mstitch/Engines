1f500653f6af67e2b1daa2af3c702382e80fe59f
mstitch <marcosvv11@gmail.com>
2020-06-12 07:30:38 -0300

    movimentação,animações - personagem,combate e cenário

---
+++ Assets/script/TPS.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPS : MonoBehaviour
+{
+    public float velocidadeMov;
+    public float deslocamentoAltura;
+    public LayerMask camadaChao;
+    public Animator anim;
+
+    Transform tr;
+    Rigidbody rb;
+    Transform trCam;
+
+    void Awake()
+    {
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+        trCam = GameObject.FindWithTag("Tripe").GetComponent<Transform> ();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = Vector3.zero;
+        bool apertouAtaque = Input.GetButtonDown("Fire1");
+        float movH = Input.GetAxis("Horizontal");
+        float movV = Input.GetAxis("Vertical");
+
+        if (apertouAtaque) { anim.SetTrigger("atacou"); }
+
+        Vector3 mov = new Vector3(movH, 0, movV);
+
+        tr.LookAt(tr.position + trCam.TransformDirection(mov) * 5);
+
+        if (mov.magnitude > 1f)
+            mov.Normalize();
+
+        tr.Translate(0, 0, mov.magnitude * velocidadeMov * Time.deltaTime);
+
+        anim.SetFloat("velocidade", mov.magnitude);
+        //anim.SetBool("estaNoChao", estaNoChao);
+
+        RaycastHit hit;
+        bool rcBateyBiChao = Physics.Raycast(
+            tr.position,
+            Vector3.down,
+            out hit,
+            Mathf.Infinity,
+            camadaChao);
+
+        if (rcBateyBiChao)
+        {
+           Vector3 pos = tr.position;
+            pos.y = hit.point.y + deslocamentoAltura;
+            tr.position = pos;
+        }
+    }
+
+
+}
+   
---
+++ Assets/script/Vida.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour
+{
+
+    public float vida;
+
+}
+   
